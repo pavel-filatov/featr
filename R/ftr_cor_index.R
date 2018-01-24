bf92085bc8470b1a1b9a43e90a0bfa02f93c7733
datanewbee <hello@datanewbee.ru>
2018-01-24 09:44:21 +0300

    cor_index function skeleton added

---
+++ R/ftr_cor_index.R
@@ -0,0 +1,13 @@
+#' Get correlation index for data frame
+#'
+#' @param .df data frame
+#'
+#' @return data frame with correllation index
+#' @export
+#'
+#' @examples
+#' ftr_cor_index(mtcars)
+#'
+ftr_cor_index <- function(.df) {
+
+}
