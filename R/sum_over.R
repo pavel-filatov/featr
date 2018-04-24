02e6e6fcfe58b125a670dbd6b5cab4679af5249f
Pavel Filatov <filatov.py@gmail.com>
2018-04-24 22:10:54 +0300

    minor changes to sum_over() to keep consistency

--- R/sum_over.R
+++ R/sum_over.R
@@ -3,7 +3,7 @@
 #' Function builds an expression to sum over selected variables.
 #' You can select variables with tidyverse way.
 #'
-#' @param .data data frame
+#' @param .df data frame
 #' @param ... variables to sum
 #'
 #' @return
@@ -15,8 +15,8 @@
 #' @examples
 #' # Sum all vars from `mpg` to `carb` except `cyl` to `am`
 #' mutate(mtcars, ss = sum_over(mtcars, mpg:carb, -(cyl:am)))
-sum_over <- function(.data, ...) {
-  .vars <- tidyselect::vars_select(names(.data), !!!rlang::quos(...))
+sum_over <- function(.df, ...) {
+  .vars <- tidyselect::vars_select(names(.df), ...)
   .vars_quoted <- rlang::as_quosures(purrr::map(.vars, as.symbol), env = rlang::caller_env())
   .expr <- purrr::reduce(.vars_quoted, ~ rlang::quo(!!.x + !!.y))
   rlang::eval_tidy(.expr)
