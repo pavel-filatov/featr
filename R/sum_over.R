64b28603158c94b4100c9084f01b92baf7a6b07d
Pavel Filatov <filatov.py@gmail.com>
2018-05-07 18:30:58 +0300

    behavior of sum_over() function stabilized

--- R/sum_over.R
+++ R/sum_over.R
@@ -9,7 +9,7 @@
 #' @return
 #' @export
 #' @importFrom tidyselect vars_select
-#' @importFrom rlang quo quos as_quosures caller_env eval_tidy
+#' @importFrom rlang quo quos as_quosure caller_env eval_tidy
 #' @importFrom purrr map reduce
 #'
 #' @examples
@@ -17,7 +17,8 @@
 #' mutate(mtcars, ss = sum_over(mtcars, mpg:carb, -(cyl:am)))
 sum_over <- function(.df, ...) {
   .vars <- tidyselect::vars_select(names(.df), ...)
-  .vars_quoted <- rlang::as_quosures(purrr::map(.vars, as.symbol), env = rlang::caller_env())
+  .vars_symbols <- purrr::map(.vars, as.symbol)
+  .vars_quoted <- purrr::map(.vars_symbols, rlang::as_quosure, env = rlang::caller_env())
   .expr <- purrr::reduce(.vars_quoted, ~ rlang::quo(!!.x + !!.y))
   rlang::eval_tidy(.expr)
 }
